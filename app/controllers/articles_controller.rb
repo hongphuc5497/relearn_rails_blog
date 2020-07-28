5859b4ab6e4cea8f8f164c16649b8e73a5ab2621
Hong Phuc <hongphuc.dthp@gmail.com>
2020-07-28 20:07:08 +0700

    Add view for index, show actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,24 @@ class ArticlesController < ApplicationController
   def new; end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+    # byebug
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
