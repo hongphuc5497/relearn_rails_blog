a00de0568d26c6a6a2efbd476eb85d985ef112b4
Hong Phuc <hongphuc.dthp@gmail.com>
2020-07-29 11:18:51 +0700

    Handle edit action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
@@ -8,23 +12,36 @@ class ArticlesController < ApplicationController
 
     if @article.save
       redirect_to @article
-    else 
+    else
       render 'new'
     end
   end
 
   def show
-    @article = Article.find(params[:id])
+    @article = query_article
   end
 
-  def index
-    @articles = Article.all
-    # byebug
+  def edit
+    @article = query_article
+  end
+
+  def update
+    @article = query_article
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      redner 'edit'
+    end
   end
 
   private
 
   def article_params
     params.require(:article).permit(:title, :text)
+  end
+
+  def query_article
+    Article.find(params[:id])
   end
 end
